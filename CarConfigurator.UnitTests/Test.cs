b18523b8f1a20f13458bd8a2be2a29fddb24e631
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-08-04 19:40:37 +0200

    Add demo unit test

---
+++ CarConfigurator.UnitTests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace CarConfigurator.UnitTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
